6fcbf0df51da3a1c3dedcb8a7d9c994de9627ffa
raphael <raphael.hudon-voyer@mail.mcgill.ca>
2014-06-21 14:24:05 -0400

    fix

--- web.js
+++ web.js
@@ -1,7 +1,10 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
 var app = connect();
-app.use(serveStatic('dist'));
-app.listen(3000);
+var port = Number(process.env.PORT || 5000);
 
-console.log('listenning to port 3000');
+app.use(serveStatic('dist'));
+app.listen(port, function () {
+	console.log('listenning to port 3000');
+	
+});
